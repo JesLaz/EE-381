5db31e7f6406041f2ef02d76368c4d91ec9daaa9
Jessie Lazo <38595549+JesLaz@users.noreply.github.com>
2020-10-16 23:03:59 -0700

    Fix solution to experiment3( ) and comment code

--- assignment3.py
+++ assignment3.py
@@ -1,7 +1,5 @@
-#Binomiall distribution
-#P(X) = (n c x)p^x * q^(n-x)
-#n = number of trials, x = number of successful attempts, p = probability, q = probabilty of failure
-
+#Jessie Lazo
+#EE381
 import numpy as np
 import matplotlib.pyplot as plt
 import random
@@ -13,7 +11,9 @@ pr = [0.2, 0.1, 0.15, 0.3, 0.2, 0.05]
 #Number of attempts for experiment 1
 n = 1000
 
-
+#Function that simulates the roll of a nSided Dice, number of sizes based off size of probability vector
+#@param -  Probability vector for a dice
+#@return - Value of the dice roll
 def nSidedDice(prb):
     n = len(prb)
     cs = np.cumsum(prb)
@@ -25,10 +25,15 @@ def nSidedDice(prb):
             break
     return d
 
+#Helper function for experiment1(), rolls 3 die, 1 time each, checking if the first dice rolls 1, the second dice rolls 2,
+#third dice rolls 3
+#return - Boolean value if trail succeeded or not
 def tripleRoll():
     if(nSidedDice(pr) == 1 and nSidedDice(pr) == 2 and nSidedDice(pr) == 3):
         return True
 
+#Helper function for experiement1(), performs tripleRoll() a given amount of instances
+#@return - number of successful attempts of tripleRoll()
 def successfulAttempts():
     sAttempt = 0
     for i in range(n):
@@ -36,11 +41,17 @@ def successfulAttempts():
             sAttempt += 1
     return sAttempt
 
-#Helper function for binomial distribution
+#Helper function for experiment2(), performs the math to find the value of n Choose r
+#@param - n: the set or population
+#@param - r: subset of n or sample set
+#@return - value of nChooseR
 def nChooseR(n, r):
     f = math.factorial
     return f(n) // f(r) // f(n-r)
 
+#Helper function for experimen2(), finds the value of the binomial distribution formula relative to x
+#@param - x: number of successes in Bernoulli trails
+#@return - probability of trials
 def binomial(x):
     #p = Probability of success; in this case its p(Dice 1 == 1, Dice 2 == 2, Dice 3 == 3) = 0.003
     p = 0.003
@@ -48,11 +59,14 @@ def binomial(x):
     result = (nChooseR(1000,x) * (p**x) * (q**(1000-x)))
     return result
 
+#Helper function for experiment3(), performs the math of the poisson distribution formula relative to x
+#@param - x: number of successes in Bernoulii trials
+#@return - probability of trials
 def poissonDist(x):
     #p = Probability of success; in this case its p(Dice 1 == 1, Dice 2 == 2, Dice 3 == 3) = 0.003
     p = 0.003
     lam = 1000*p
-    result = (lam**x)**(math.e**(-lam)) / (math.factorial(x))
+    result = (lam**x)*(math.e**(-lam)) / (math.factorial(x))
     return result
 
 def experiment1():
@@ -90,7 +104,7 @@ def experiment3():
     plt.show()
 
 def main():
-    #experiment1()
+    experiment1()
     experiment2()
     experiment3()
 
